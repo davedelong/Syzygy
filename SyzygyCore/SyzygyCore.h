97812739a0e1b456ff202858ce32d8b020fa689d
Dave DeLong <davedelong@me.com>
2017-12-31 17:18:04 -0700

    Initial Commit

---
+++ SyzygyCore/SyzygyCore.h
@@ -0,0 +1,19 @@
+//
+//  SyzygyCore.h
+//  SyzygyCore
+//
+//  Created by Dave DeLong on 12/31/17.
+//  Copyright © 2017 Syzygy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SyzygyCore.
+FOUNDATION_EXPORT double SyzygyCoreVersionNumber;
+
+//! Project version string for SyzygyCore.
+FOUNDATION_EXPORT const unsigned char SyzygyCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SyzygyCore/PublicHeader.h>
+
+
