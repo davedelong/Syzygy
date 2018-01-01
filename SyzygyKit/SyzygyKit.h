b901466b407918282ab53d6ebc4cf349b844c1e1
Dave DeLong <davedelong@me.com>
2017-12-31 17:21:48 -0700

    Empty project with 2 targets

---
+++ SyzygyKit/SyzygyKit.h
@@ -0,0 +1,30 @@
+//
+//  SyzygyKit.h
+//  SyzygyKit
+//
+//  Created by Dave DeLong on 12/31/17.
+//  Copyright © 2017 Syzygy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <SyzygyCore/SyzygyCore.h>
+
+#if TARGET_OS_IPHONE
+
+#import <UIKit/UIKit.h>
+
+#else
+
+#import <AppKit/AppKit.h>
+
+#endif
+
+//! Project version number for SyzygyKit.
+FOUNDATION_EXPORT double SyzygyKitVersionNumber;
+
+//! Project version string for SyzygyKit.
+FOUNDATION_EXPORT const unsigned char SyzygyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SyzygyKit/PublicHeader.h>
+
+
