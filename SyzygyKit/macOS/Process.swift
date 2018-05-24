573b42c50fd919238ffb737048a4dc2d55d791d1
Dave DeLong <davedelong@me.com>
2018-05-24 16:21:38 -0600

    Process.transform(to:)

---
+++ SyzygyKit/macOS/Process.swift
@@ -0,0 +1,45 @@
+//
+//  Process.swift
+//  SyzygyKit
+//
+//  Created by Dave DeLong on 5/24/18.
+//  Copyright © 2018 Syzygy. All rights reserved.
+//
+
+import Cocoa
+
+public extension Process {
+    
+    public enum Type {
+        /// Foreground applications have a menu bar and appear in the Dock
+        case foreground
+        
+        /// Background applications do not have a menu bar nor appear in the Dock.
+        /// These kinds of applications should not have windows or other UI elements.
+        case background
+        
+        /// UIElement applications do not have a menu bar, nor appear in the Dock.
+        /// These kinds of applications may present windows and other UI.
+        case UIElement
+    }
+    
+    
+    /// Transform the type of the current process
+    ///
+    /// - Parameter processType: The `Type` of the process. This should usually be either `.foreground` or `.UIElement`.
+    /// - Returns: `true` if the transformation was successful, and `false` otherwise.
+    @discardableResult
+    public func transform(to processType: Type) -> Bool {
+        let state: ProcessApplicationTransformState
+        switch processType {
+            case .foreground: state = kProcessTransformToForegroundApplication
+            case .background: state = kProcessTransformToBackgroundApplication
+            case .UIElement: state = kProcessTransformToUIElementApplication
+        }
+        
+        var psn = ProcessSerialNumber(highLongOfPSN: 0, lowLongOfPSN: UInt32(kCurrentProcess))
+        let status = TransformProcessType(&psn, state)
+        return state == noErr
+    }
+    
+}
