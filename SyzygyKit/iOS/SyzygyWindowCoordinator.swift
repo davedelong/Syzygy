729f8281084c03549af4a00b8783cab2553d2f84
Dave DeLong <davedelong@me.com>
2018-07-06 16:56:39 -0600

    Rename + WindowCoordinator

---
+++ SyzygyKit/iOS/SyzygyWindowCoordinator.swift
@@ -0,0 +1,42 @@
+//
+//  SyzygyWindowCoordinator.swift
+//  SyzygyKit
+//
+//  Created by Dave DeLong on 7/4/18.
+//  Copyright © 2018 Syzygy. All rights reserved.
+//
+
+import SyzygyCore
+
+open class SyzygyWindowCoordinator: Coordinator {
+    
+    private var disposable = CompositeDisposable()
+    
+    private lazy var window: UIWindow = {
+        let w = UIWindow(frame: UIScreen.main.bounds)
+        w.rootViewController = container
+        return w
+    }()
+    
+    private let container: SyzygyContainerViewController
+    
+    public init(content: Property<UIViewController>) {
+        container = SyzygyContainerViewController(content: content)
+        super.init()
+    }
+    
+    public convenience init(content: UIViewController) {
+        self.init(content: Property(content))
+    }
+    
+    open override func start() {
+        super.start()
+        window.isHidden = false
+        window.makeKeyAndVisible()
+    }
+    
+    open override func stop() {
+        window.resignKey()
+        window.isHidden = true
+    }
+}
