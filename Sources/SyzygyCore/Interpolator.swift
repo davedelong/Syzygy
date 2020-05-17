a6634f458a93d52b5d6b9a33302593fa48d903bb
Dave DeLong <davedelong@me.com>
2020-05-17 16:02:13 -0600

    more scaling

--- Sources/SyzygyCore/Interpolator.swift
+++ Sources/SyzygyCore/Interpolator.swift
@@ -17,11 +17,25 @@ public struct Interpolator {
                             reverseInterpolate: { ($0 * span) + lower })
     }
     
-    public static func logarithmic(_ range: ClosedRange<Double>) -> Interpolator {
+    public static func logarithmic(_ range: ClosedRange<Double>, scale: Double = 1.0) -> Interpolator {
         let logMin = log1p(range.lowerBound)
         let logRange = log1p(range.upperBound) - log1p(range.lowerBound)
-        return Interpolator(interpolate: { (log1p($0) - logMin) / logRange },
-                            reverseInterpolate: { expm1(($0 * logRange) + logMin) })
+        
+        return Interpolator(interpolate: {
+                                if $0 == range.lowerBound { return 0 }
+                                if $0 == range.upperBound { return 1 }
+                                let interpolated = (log1p($0) - logMin) / logRange
+                                if scale <= 0 || scale == 1 { return interpolated }
+                                return pow(interpolated, 1.0 / scale)
+                            },
+                            reverseInterpolate: {
+                                if $0 == 0 { return range.lowerBound }
+                                if $0 == 1 { return range.upperBound }
+                                var input = $0
+                                if scale > 0 && scale != 1 { input = pow($0, scale) }
+                                return expm1((input * logRange) + logMin)
+                            }
+        )
     }
     
     public let interpolate: (Double) -> Double
