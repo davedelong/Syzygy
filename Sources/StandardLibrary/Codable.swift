ea4e80d9ecb46fa3f086f05f1c3bd45dae80474a
Dave DeLong <davedelong@me.com>
2020-09-07 15:14:16 -0600

    Codable

--- Sources/StandardLibrary/Codable.swift
+++ Sources/StandardLibrary/Codable.swift
@@ -8,6 +8,36 @@
 
 import Foundation
 
+public struct AnyCodingKey: CodingKey {
+    
+    public var stringValue: String
+    
+    public init?(stringValue: String) {
+        self.stringValue = stringValue
+        self.intValue = nil
+    }
+    
+    public var intValue: Int?
+    
+    public init?(intValue: Int) {
+        self.intValue = intValue
+        self.stringValue = "\(intValue)"
+    }
+    
+}
+
+extension AnyCodingKey: ExpressibleByIntegerLiteral, ExpressibleByStringLiteral {
+    public init(integerLiteral value: Int) {
+        self.intValue = value
+        self.stringValue = "\(value)"
+    }
+    
+    public init(stringLiteral value: String) {
+        self.intValue = nil
+        self.stringValue = value
+    }
+}
+
 fileprivate func prettyPath(_ parts: Array<CodingKey>) -> String {
     let components = parts.enumerated().map { (offset, key) -> String in
         if let idx = key.intValue {
